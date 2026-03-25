47b6b65df168aec7c7e7f3e2aa75e56eec4b2cb3
Joseph Ibok <josephibok75@gmail.com>
2026-03-25 15:28:23 +0100

    Initilized Blog API

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local
+    'accounts.apps.AccountsConfig',
+    'posts.apps.PostsConfig',
+    'comments.apps.CommentsConfig',
 ]
 
 MIDDLEWARE = [
